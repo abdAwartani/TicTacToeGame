d29bd9c6500c265ab17b9bfbfc1ffbfb30902217
Abdallah Awartani <AbdallahA@leading-point.com>
2020-05-05 12:55:03 +0300

    Add project files.

---
+++ TicTakTokGame/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace TicTakTokGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.x1 = new System.Windows.Forms.Button();
+            this.x2 = new System.Windows.Forms.Button();
+            this.x3 = new System.Windows.Forms.Button();
+            this.y1 = new System.Windows.Forms.Button();
+            this.y2 = new System.Windows.Forms.Button();
+            this.y3 = new System.Windows.Forms.Button();
+            this.z1 = new System.Windows.Forms.Button();
+            this.z2 = new System.Windows.Forms.Button();
+            this.z3 = new System.Windows.Forms.Button();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.lblOPLayer = new System.Windows.Forms.Label();
+            this.lblXPlayer = new System.Windows.Forms.Label();
+            this.lblO = new System.Windows.Forms.Label();
+            this.lblX = new System.Windows.Forms.Label();
+            this.lblPlayer = new System.Windows.Forms.Label();
+            this.lblTurnPlayer = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.twoPlayer = new System.Windows.Forms.Button();
+            this.cpuPlayer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // x1
+            // 
+            this.x1.Location = new System.Drawing.Point(143, 70);
+            this.x1.Name = "x1";
+            this.x1.Size = new System.Drawing.Size(80, 75);
+            this.x1.TabIndex = 0;
+            this.x1.UseVisualStyleBackColor = true;
+            this.x1.Click += new System.EventHandler(this.x1_Click);
+            // 
+            // x2
+            // 
+            this.x2.Location = new System.Drawing.Point(262, 70);
+            this.x2.Name = "x2";
+            this.x2.Size = new System.Drawing.Size(80, 75);
+            this.x2.TabIndex = 1;
+            this.x2.UseVisualStyleBackColor = true;
+            this.x2.Click += new System.EventHandler(this.x2_Click);
+            // 
+            // x3
+            // 
+            this.x3.Location = new System.Drawing.Point(382, 70);
+            this.x3.Name = "x3";
+            this.x3.Size = new System.Drawing.Size(80, 75);
+            this.x3.TabIndex = 2;
+            this.x3.UseVisualStyleBackColor = true;
+            this.x3.Click += new System.EventHandler(this.x3_Click);
+            // 
+            // y1
+            // 
+            this.y1.Location = new System.Drawing.Point(143, 164);
+            this.y1.Name = "y1";
+            this.y1.Size = new System.Drawing.Size(80, 75);
+            this.y1.TabIndex = 3;
+            this.y1.UseVisualStyleBackColor = true;
+            this.y1.Click += new System.EventHandler(this.y1_Click);
+            // 
+            // y2
+            // 
+            this.y2.Location = new System.Drawing.Point(262, 164);
+            this.y2.Name = "y2";
+            this.y2.Size = new System.Drawing.Size(80, 75);
+            this.y2.TabIndex = 4;
+            this.y2.UseVisualStyleBackColor = true;
+            this.y2.Click += new System.EventHandler(this.y2_Click);
+            // 
+            // y3
+            // 
+            this.y3.Location = new System.Drawing.Point(382, 164);
+            this.y3.Name = "y3";
+            this.y3.Size = new System.Drawing.Size(80, 75);
+            this.y3.TabIndex = 5;
+            this.y3.UseVisualStyleBackColor = true;
+            this.y3.Click += new System.EventHandler(this.y3_Click);
+            // 
+            // z1
+            // 
+            this.z1.Location = new System.Drawing.Point(143, 268);
+            this.z1.Name = "z1";
+            this.z1.Size = new System.Drawing.Size(80, 75);
+            this.z1.TabIndex = 6;
+            this.z1.UseVisualStyleBackColor = true;
+            this.z1.Click += new System.EventHandler(this.z1_Click);
+            // 
+            // z2
+            // 
+            this.z2.Location = new System.Drawing.Point(262, 268);
+            this.z2.Name = "z2";
+            this.z2.Size = new System.Drawing.Size(80, 75);
+            this.z2.TabIndex = 7;
+            this.z2.UseVisualStyleBackColor = true;
+            this.z2.Click += new System.EventHandler(this.z2_Click);
+            // 
+            // z3
+            // 
+            this.z3.Location = new System.Drawing.Point(382, 268);
+            this.z3.Name = "z3";
+            this.z3.Size = new System.Drawing.Size(80, 75);
+            this.z3.TabIndex = 8;
+            this.z3.UseVisualStyleBackColor = true;
+            this.z3.Click += new System.EventHandler(this.z3_Click);
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Location = new System.Drawing.Point(619, 84);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(123, 46);
+            this.resetBtn.TabIndex = 9;
+            this.resetBtn.Text = "Reset";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.Click += new System.EventHandler(this.resetBtn_Click);
+            // 
+            // lblOPLayer
+            // 
+            this.lblOPLayer.AutoSize = true;
+            this.lblOPLayer.Location = new System.Drawing.Point(526, 271);
+            this.lblOPLayer.Name = "lblOPLayer";
+            this.lblOPLayer.Size = new System.Drawing.Size(71, 17);
+            this.lblOPLayer.TabIndex = 10;
+            this.lblOPLayer.Text = "Player O :";
+            // 
+            // lblXPlayer
+            // 
+            this.lblXPlayer.AutoSize = true;
+            this.lblXPlayer.Location = new System.Drawing.Point(526, 318);
+            this.lblXPlayer.Name = "lblXPlayer";
+            this.lblXPlayer.Size = new System.Drawing.Size(69, 17);
+            this.lblXPlayer.TabIndex = 11;
+            this.lblXPlayer.Text = "Player X :";
+            // 
+            // lblO
+            // 
+            this.lblO.AutoSize = true;
+            this.lblO.Location = new System.Drawing.Point(603, 271);
+            this.lblO.Name = "lblO";
+            this.lblO.Size = new System.Drawing.Size(0, 17);
+            this.lblO.TabIndex = 12;
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Location = new System.Drawing.Point(603, 318);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(0, 17);
+            this.lblX.TabIndex = 13;
+            // 
+            // lblPlayer
+            // 
+            this.lblPlayer.AutoSize = true;
+            this.lblPlayer.Location = new System.Drawing.Point(524, 164);
+            this.lblPlayer.Name = "lblPlayer";
+            this.lblPlayer.Size = new System.Drawing.Size(85, 17);
+            this.lblPlayer.TabIndex = 14;
+            this.lblPlayer.Text = "Player turn :";
+            // 
+            // lblTurnPlayer
+            // 
+            this.lblTurnPlayer.AutoSize = true;
+            this.lblTurnPlayer.Location = new System.Drawing.Point(616, 164);
+            this.lblTurnPlayer.Name = "lblTurnPlayer";
+            this.lblTurnPlayer.Size = new System.Drawing.Size(0, 17);
+            this.lblTurnPlayer.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(606, 414);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 17);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "(c) 2020  Abdullah Awartani";
+            // 
+            // twoPlayer
+            // 
+            this.twoPlayer.Location = new System.Drawing.Point(12, 12);
+            this.twoPlayer.Name = "twoPlayer";
+            this.twoPlayer.Size = new System.Drawing.Size(123, 46);
+            this.twoPlayer.TabIndex = 17;
+            this.twoPlayer.Text = "1P vs 2P";
+            this.twoPlayer.UseVisualStyleBackColor = true;
+            this.twoPlayer.Click += new System.EventHandler(this.twoPlayer_Click);
+            // 
+            // cpuPlayer
+            // 
+            this.cpuPlayer.Location = new System.Drawing.Point(141, 12);
+            this.cpuPlayer.Name = "cpuPlayer";
+            this.cpuPlayer.Size = new System.Drawing.Size(123, 46);
+            this.cpuPlayer.TabIndex = 18;
+            this.cpuPlayer.Text = "1P vs CPU";
+            this.cpuPlayer.UseVisualStyleBackColor = true;
+            this.cpuPlayer.Click += new System.EventHandler(this.cpuPlayer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cpuPlayer);
+            this.Controls.Add(this.twoPlayer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTurnPlayer);
+            this.Controls.Add(this.lblPlayer);
+            this.Controls.Add(this.lblX);
+            this.Controls.Add(this.lblO);
+            this.Controls.Add(this.lblXPlayer);
+            this.Controls.Add(this.lblOPLayer);
+            this.Controls.Add(this.resetBtn);
+            this.Controls.Add(this.z3);
+            this.Controls.Add(this.z2);
+            this.Controls.Add(this.z1);
+            this.Controls.Add(this.y3);
+            this.Controls.Add(this.y2);
+            this.Controls.Add(this.y1);
+            this.Controls.Add(this.x3);
+            this.Controls.Add(this.x2);
+            this.Controls.Add(this.x1);
+            this.Name = "Form1";
+            this.Text = "TicTakToc";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button x1;
+        private System.Windows.Forms.Button x2;
+        private System.Windows.Forms.Button x3;
+        private System.Windows.Forms.Button y1;
+        private System.Windows.Forms.Button y2;
+        private System.Windows.Forms.Button y3;
+        private System.Windows.Forms.Button z1;
+        private System.Windows.Forms.Button z2;
+        private System.Windows.Forms.Button z3;
+        private System.Windows.Forms.Button resetBtn;
+        private System.Windows.Forms.Label lblOPLayer;
+        private System.Windows.Forms.Label lblXPlayer;
+        private System.Windows.Forms.Label lblO;
+        private System.Windows.Forms.Label lblX;
+        private System.Windows.Forms.Label lblPlayer;
+        private System.Windows.Forms.Label lblTurnPlayer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button twoPlayer;
+        private System.Windows.Forms.Button cpuPlayer;
+    }
+}
+
---
+++ TicTakTokGame/Form1.cs
@@ -0,0 +1,421 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTakTokGame
+{
+    public partial class Form1 : Form
+    {
+        #region Constructor
+        public Form1()
+        {
+            InitializeComponent();
+            FillAllButtons();
+            Buttons.Reverse();
+            Btns = new Dictionary<int, string>();
+            Buttons.ForEach(btn => { Btns.Add(Buttons.IndexOf(btn) + 1, btn.Name); });
+
+            Reset();
+        }
+        #endregion
+
+        #region Properties
+        public bool Oplayer { get; set; }
+        public int XPlayerCounter { get; set; }
+        public int OPlayerCounter { get; set; }
+        private int BtnCounter { get; set; }
+        public bool IsCPU { get; set; }
+        public bool CPUTrun { get; set; }
+        public Dictionary<int, string> Btns { get; set; }
+        private int Postion { get; set; }
+
+        private List<Button> Buttons { get; set; }
+        #endregion
+
+        #region Events
+        private void x1_Click(object sender, EventArgs e)
+        {
+            ChoosePostion(HandleClick(sender));
+        }
+        private void x2_Click(object sender, EventArgs e)
+        {
+            ChoosePostion(HandleClick(sender));
+        }
+        private void x3_Click(object sender, EventArgs e)
+        {
+            ChoosePostion(HandleClick(sender));
+        }
+        private void y1_Click(object sender, EventArgs e)
+        {
+            ChoosePostion(HandleClick(sender));
+        }
+        private void y2_Click(object sender, EventArgs e)
+        {
+            ChoosePostion(HandleClick(sender));
+        }
+        private void y3_Click(object sender, EventArgs e)
+        {
+            ChoosePostion(HandleClick(sender));
+        }
+        private void z1_Click(object sender, EventArgs e)
+        {
+            ChoosePostion(HandleClick(sender));
+        }
+        private void z2_Click(object sender, EventArgs e)
+        {
+            ChoosePostion(HandleClick(sender));
+        }
+        private void z3_Click(object sender, EventArgs e)
+        {
+            ChoosePostion(HandleClick(sender));
+        }
+        private void resetBtn_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+        private void twoPlayer_Click(object sender, EventArgs e)
+        {
+            Reset();
+            IsCPU = false;
+            PlayerMode();
+        }
+        private void cpuPlayer_Click(object sender, EventArgs e)
+        {
+            Reset();
+            IsCPU = true;
+            PlayerMode();
+        }
+        private TicBtn HandleClick(object sender)
+        {
+            TicBtn ticBtn = new TicBtn();
+            ticBtn.button = sender as Button;
+            return ticBtn;
+        }
+        #endregion
+
+        #region Methods
+        private void ChoosePostion(TicBtn ticBtn)
+        {
+            BtnCounter++;
+            var _Winner = string.Empty;
+            if (IsCPU && CPUTrun)
+            {
+                Postion = 0;
+                var button = CPUDecesion();
+                button.Text = Oplayer ? "O" : "X";
+                button.BackColor = Oplayer ? Color.Blue : Color.Green;
+                button.Enabled = false;
+            }
+            else
+            {
+                ticBtn.button.Text = Oplayer ? "O" : "X";
+                ticBtn.button.BackColor = Oplayer ? Color.Blue : Color.Green;
+                ticBtn.button.Enabled = false;
+            }
+            if (IsCPU) CPUTrun = !CPUTrun;
+            Oplayer = !Oplayer;
+            lblTurnPlayer.Text = Oplayer ? "O Player" : "X Player";
+            _Winner = EvaluateWinner();
+            if (!string.IsNullOrWhiteSpace(_Winner))
+            {
+                if (_Winner == "O")
+                {
+                    OPlayerCounter++;
+                    lblO.Text = WinCounter("O");
+                }
+                else
+                {
+                    XPlayerCounter++;
+                    lblX.Text = WinCounter("X");
+                }
+                MessageBox.Show($"Player {_Winner} Wins!");
+                ContinueGame();
+            }
+
+            if (BtnCounter == 8)
+            {
+                MessageBox.Show("Draw !");
+                ContinueGame();
+            }
+            if (IsCPU && CPUTrun) ChoosePostion(new TicBtn());
+        }
+        private void ContinueGame()
+        {
+            BtnCounter = 0;
+            Oplayer = OPlayerCounter > XPlayerCounter ? true : false;
+            CPUTrun = !IsCPU ? false : !Oplayer;
+            lblTurnPlayer.Text = Oplayer ? "O Player" : "X Player";
+            x1.Enabled = x2.Enabled = x3.Enabled = y1.Enabled = y2.Enabled = y3.Enabled = z1.Enabled = z2.Enabled = z3.Enabled = true;
+            x1.Text = x2.Text = x3.Text = y1.Text = y2.Text = y3.Text = z1.Text = z2.Text = z3.Text = "";
+            x1.BackColor = x2.BackColor = x3.BackColor = y1.BackColor = y2.BackColor = y3.BackColor = z1.BackColor = z2.BackColor = z3.BackColor = Color.White;
+            if (CPUTrun) ChoosePostion(new TicBtn());
+        }
+        private void Reset()
+        {
+            CPUTrun = false;
+            IsCPU = false;
+            BtnCounter = 0;
+            Oplayer = true;
+            lblO.Text = "0";
+            lblX.Text = "0";
+            lblTurnPlayer.Text = Oplayer ? "O Player" : "X Player";
+            x1.Enabled = x2.Enabled = x3.Enabled = y1.Enabled = y2.Enabled = y3.Enabled = z1.Enabled = z2.Enabled = z3.Enabled = true;
+            x1.Text = x2.Text = x3.Text = y1.Text = y2.Text = y3.Text = z1.Text = z2.Text = z3.Text = "";
+            x1.BackColor = x2.BackColor = x3.BackColor = y1.BackColor = y2.BackColor = y3.BackColor = z1.BackColor = z2.BackColor = z3.BackColor = Color.White;
+            PlayerMode();
+        }
+        private string WinCounter(string player)
+        {
+            string _counter = string.Empty;
+            switch (player)
+            {
+                case "X":
+                    _counter = XPlayerCounter + " Wins";
+                    break;
+                case "O":
+                    _counter = OPlayerCounter + " Wins";
+                    break;
+            }
+            return _counter;
+        }
+        private string EvaluateWinner()
+        {
+            string _winner = string.Empty;
+
+            if (x1.Text == x2.Text && x1.Text == x3.Text)
+            {
+                _winner = x1.Text;
+            }
+            else if (y1.Text == y2.Text && y1.Text == y3.Text)
+            {
+                _winner = y1.Text;
+            }
+            else if (z1.Text == z2.Text && z1.Text == z3.Text)
+            {
+                _winner = z1.Text;
+            }
+            else if (x1.Text == y1.Text && y1.Text == z1.Text)
+            {
+                _winner = x1.Text;
+            }
+            else if (x2.Text == y2.Text && y2.Text == z2.Text)
+            {
+                _winner = x2.Text;
+            }
+            else if (x3.Text == y3.Text && y3.Text == z3.Text)
+            {
+                _winner = x3.Text;
+            }
+            else if (x1.Text == y2.Text && y2.Text == z3.Text)
+            {
+                _winner = x1.Text;
+            }
+            else if (x3.Text == y2.Text && y2.Text == z1.Text)
+            {
+                _winner = x3.Text;
+            }
+
+            return _winner;
+        }
+        private Control CPUDecesion()
+        {
+            var _ctrlId = string.Empty;
+            var _pred = WinMoveForCPU();
+            if (_pred > 0)
+            {
+                _ctrlId = Btns.Where(b => b.Key == _pred).Select(v => v.Value).FirstOrDefault();
+                if (CheckValidPostion(_ctrlId))
+                {
+                    return Buttons.Where(b => b.Name == _ctrlId).FirstOrDefault();
+                }
+                else
+                {
+                    return PickPostion();
+                }
+            }
+            else
+            {
+                return PickPostion();
+            }
+        }
+        private Control PickPostion()
+        {
+            var _postion = ++Postion;
+            var _ctrlId = Btns.Where(b => b.Key == _postion).Select(v => v.Value).FirstOrDefault();
+            if (CheckValidPostion(_ctrlId))
+            {
+                return Buttons.Where(b => b.Name == _ctrlId).FirstOrDefault();
+            }
+            else
+            {
+                return PickPostion();
+            }
+        }
+        private bool CheckValidPostion(string postion)
+        {
+            return Buttons.Any(b => b.Name == postion && b.Enabled && string.IsNullOrEmpty(b.Text));
+        }
+        private void PlayerMode()
+        {
+            if (IsCPU)
+            {
+                cpuPlayer.BackColor = Color.Green;
+                twoPlayer.BackColor = Color.White;
+            }
+            else
+            {
+                cpuPlayer.BackColor = Color.White;
+                twoPlayer.BackColor = Color.Green;
+            }
+        }
+        private int WinMoveForCPU()
+        {
+            var Xs = new List<string>();
+            Buttons.ForEach(btn =>
+            {
+                if (btn.Text == "X") Xs.Add(btn.Name);
+            });
+
+            #region Raws
+            //X Raw
+            if (Xs.Contains("x3") && Xs.Contains("x2") && string.IsNullOrEmpty(x1.Text))
+            {
+                return 1;
+            }
+            else if (Xs.Contains("x3") && Xs.Contains("x1") && string.IsNullOrEmpty(x2.Text))
+            {
+                return 2;
+            }
+            else if (Xs.Contains("x1") && Xs.Contains("x2") && string.IsNullOrEmpty(x3.Text))
+            {
+                return 3;
+            }
+            //Y raw
+            else if (Xs.Contains("y3") && Xs.Contains("y2") && string.IsNullOrEmpty(y1.Text))
+            {
+                return 4;
+            }
+            else if (Xs.Contains("y3") && Xs.Contains("y1") && string.IsNullOrEmpty(y2.Text))
+            {
+                return 5;
+            }
+            else if (Xs.Contains("y2") && Xs.Contains("x1") && string.IsNullOrEmpty(y3.Text))
+            {
+                return 6;
+            }
+            //Z raw
+            else if (Xs.Contains("z3") && Xs.Contains("z2") && string.IsNullOrEmpty(z1.Text))
+            {
+                return 7;
+            }
+            else if (Xs.Contains("z3") && Xs.Contains("z1") && string.IsNullOrEmpty(z2.Text))
+            {
+                return 8;
+            }
+            else if (Xs.Contains("z2") && Xs.Contains("z1") && string.IsNullOrEmpty(z3.Text))
+            {
+                return 9;
+            }
+            #endregion
+            #region Cols
+            //Col 1
+            else if (Xs.Contains("x1") && Xs.Contains("y1") && string.IsNullOrEmpty(z1.Text))
+            {
+                return 7;
+            }
+            else if (Xs.Contains("z1") && Xs.Contains("x1") && string.IsNullOrEmpty(y1.Text))
+            {
+                return 4;
+            }
+            else if (Xs.Contains("z1") && Xs.Contains("y1") && string.IsNullOrEmpty(x1.Text))
+            {
+                return 1;
+            }
+            //col 2
+            else if (Xs.Contains("x2") && Xs.Contains("z2") && string.IsNullOrEmpty(y2.Text))
+            {
+                return 5;
+            }
+            else if (Xs.Contains("x2") && Xs.Contains("y2") && string.IsNullOrEmpty(z2.Text))
+            {
+                return 8;
+            }
+            else if (Xs.Contains("z2") && Xs.Contains("y2") && string.IsNullOrEmpty(x2.Text))
+            {
+                return 2;
+            }
+            //col 3
+            else if (Xs.Contains("z3") && Xs.Contains("x3") && string.IsNullOrEmpty(y3.Text))
+            {
+                return 6;
+            }
+            else if (Xs.Contains("z3") && Xs.Contains("y3") && string.IsNullOrEmpty(x3.Text))
+            {
+                return 3;
+            }
+            else if (Xs.Contains("x3") && Xs.Contains("y3") && string.IsNullOrEmpty(z3.Text))
+            {
+                return 9;
+            }
+            #endregion
+            #region Diagonal
+            //Diagonal One
+            else if (Xs.Contains("z3") && Xs.Contains("y2") && string.IsNullOrEmpty(x1.Text))
+            {
+                return 1;
+            }
+            else if (Xs.Contains("z3") && Xs.Contains("x1") && string.IsNullOrEmpty(y2.Text))
+            {
+                return 5;
+            }
+            else if (Xs.Contains("x1") && Xs.Contains("y2") && string.IsNullOrEmpty(z3.Text))
+            {
+                return 9;
+            }
+            //Diagonal Two
+            else if (Xs.Contains("x3") && Xs.Contains("y2") && string.IsNullOrEmpty(z1.Text))
+            {
+                return 7;
+            }
+            else if (Xs.Contains("z1") && Xs.Contains("x3") && string.IsNullOrEmpty(y2.Text))
+            {
+                return 5;
+            }
+            else if (Xs.Contains("z1") && Xs.Contains("y2") && string.IsNullOrEmpty(x3.Text))
+            {
+                return 3;
+            }
+            #endregion
+
+            return -1;
+        }
+
+        private void FillAllButtons()
+        {
+            Buttons = new List<Button>();
+            // Add Global property for all buttons control
+            foreach (var control in this.FindForm().Controls)
+            {
+                var _btn = control as Control;
+                if (_btn is Button)
+                {
+                    if ((_btn.Name.StartsWith("x") || _btn.Name.StartsWith("y") || _btn.Name.StartsWith("z")) && _btn.Name.Length == 2) Buttons.Add(_btn as Button);
+                }
+            }
+        }
+        #endregion
+    }
+
+    public class TicBtn
+    {
+        public System.Windows.Forms.Button button { get; set; }
+        public string Postion { get; set; }
+        public string Player { get; set; }
+    }
+}
